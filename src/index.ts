6143243a2a2ed253f0429608e80ca211d8d326b9
Jonathan Barreira Da Silva Nunes <jbnunes@sfiec.org.br>
2020-09-23 15:43:27 -0300

    websocket funcionando.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import express, {json} from 'express';
+import {Server} from 'http';
+import socketIo from 'socket.io';
 
 const app = express();
-app.use(json());
+const http = new Server(app);
+const io = socketIo(http);
 
-app.get('/', (req, res) => {
+app.use(json());
+app.use(express.static('wwwroot'));
+
+app.get('/hello', (req, res) => {
   res.json({message: 'Hello World!'});
 });
 
-app.listen(3000);
+io.on('connection', socket => {
+  console.log(`Usuario entrou: ${socket.id}`);
+
+  const dt = new Date();
+  const dtExt = `[${dt.getDay()}/${dt.getMonth()}/${dt.getFullYear()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}]: `;
+
+  socket.on('message', message => {
+    console.log(message);
+
+    socket.emit('message', dtExt + message);
+  });
+});
+
+//app.listen(3000);
+http.listen(3000);
